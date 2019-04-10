ad3a47ffedf1a9cf224e634b34f6e1c58f289ba9
Thomas Letan <thomas.letan@ssi.gouv.fr>
2019-04-10 14:40:25 +0200

    tactics: Add a new tactic [ssubst]
    This tactic is useful to handle inversions which produces hypotheses
    of the form [existT _ _ x = existT _ _ y].

--- theories/Tactics.v
+++ theories/Tactics.v
@@ -1,5 +1,18 @@
+Require Import Coq.Logic.Eqdep.
+
 (** [dup H H'] will duplicate the hypothesis [H], under the name [H'].
     *)
 Ltac dup H H' :=
   let T := type of H in
   assert (H': T) by exact H.
+
+(** [ssubst] will deal with hypotheses of the form [existT _ _ x =
+    existT _ _ y] *)
+Ltac ssubst :=
+  lazymatch goal with
+| [ H : existT _ _ _ = existT _ _ _ |- _ ]
+  => apply Eqdep.EqdepTheory.inj_pair2 in H;
+     ssubst
+| [ |- _]
+  => subst
+end.
