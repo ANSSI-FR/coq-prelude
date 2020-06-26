19cd82f8a5b184d7d84be299ccc8c0d63dd09935
Thomas Letan <thomas.letan@ssi.gouv.fr>
2020-06-26 11:23:19 +0200

    Only copy the inner string when necessary

--- src/bytestring.ml
+++ src/bytestring.ml
@@ -1,14 +1,14 @@
 open Option
 
-type t = { offset : int ; size : int ; data : string }
+type t = { offset : int ; size : int ; data : string ; sub : bool }
 
-let of_string b = { offset = 0; size = String.length b; data = b }
+let of_string b = { offset = 0; size = String.length b; data = b ; sub = false }
 
-let to_string b = String.sub b.data b.offset b.size
+let to_string b = if b.sub then String.sub b.data b.offset b.size else b.data
 
 let shift b =
   if 0 < b.size
-  then Some { offset = b.offset + 1 ; size = b.size - 1 ; data = b.data }
+  then Some { offset = b.offset + 1 ; size = b.size - 1 ; data = b.data ; sub = true }
   else None
 
 let unsafe_get_char b x =
@@ -29,7 +29,7 @@ let unpack b =
 let pack (c, b) =
   let init = fun i -> if i == 0 then c else unsafe_get_char b (i - 1) in
   let buffer = String.init (b.size + 1) init in
-  { offset = 0 ; data = buffer ; size = b.size + 1 }
+  { offset = 0 ; data = buffer ; size = b.size + 1 ; sub = false }
 
 let case f1 f0 b =
   match unpack b with
@@ -37,7 +37,7 @@ let case f1 f0 b =
   | _ -> f0 ()
 
 let empty =
-  { offset = 0 ; data = "" ; size = 0 }
+  { offset = 0 ; data = "" ; size = 0 ; sub = false }
 
 let append b1 b2 =
   let init = fun i ->
@@ -45,7 +45,7 @@ let append b1 b2 =
     then String.get b1.data (b1.offset + i)
     else String.get b2.data (b2.offset + (i - b1.size)) in
   let buffer = String.init (b1.size + b2.size) init in
-  { offset = 0; size = b1.size + b2.size; data = buffer }
+  { offset = 0; size = b1.size + b2.size; data = buffer ; sub = false }
 
 let length b = b.size
 
@@ -55,8 +55,8 @@ let int_of_bytestring b = int_of_string_opt b.data
 
 let split b x =
   if x <= b.size
-  then Some ({ offset = b.offset; size = x; data = b.data },
-             { offset = b.offset + x; size = b.size - x; data = b.data })
+  then Some ({ offset = b.offset; size = x; data = b.data ; sub = true },
+             { offset = b.offset + x; size = b.size - x; data = b.data ; sub = true })
   else None
 
 let of_list l = List.to_seq l |> String.of_seq |> of_string
